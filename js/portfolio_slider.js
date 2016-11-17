dcf05cf15e133eb56d2349238a8bb7bcccc1e3fe
mytrod <dmytro.doianov@gmail.com>
2016-11-17 23:04:01 +0200

    My site 1.0

---
+++ js/portfolio_slider.js
@@ -0,0 +1,213 @@
+$(function(){
+$("#elastic_grid_demo").elastic_grid({	
+	'hoverDirection': true,
+	'hoverDelay': 0,
+	'hoverInverse': false,
+	'expandingSpeed': 500,
+	'expandingHeight': 500,
+	'items' :
+		[
+			{
+			'title' : 'Coming soon',
+			'description'   : 'My next work ',
+			'thumbnail' : ['img/portfolio/small/css.jpg'],
+			'large' : ['img/portfolio/large/cs.jpg'],
+			'button_list'   :
+			[
+			{ 'title':'Demo', 'url' : 'http://dmytrodoianov.github.io/demo/index.html' }
+			],
+			'tags'  : ['All']
+			},
+			{
+			'title' : 'Website in apple style',
+			'description'   : 'Website in apple style.',
+			'thumbnail' : ['img/portfolio/small/s2s.jpg', 'img/portfolio/small/s2s.jpg'],
+			'large' : ['img/portfolio/large/s2.jpg', 'img/portfolio/large/s2.jpg'],
+			'button_list'   :
+			[
+			{ 'title':'Demo', 'url' : 'http://#' }
+			],
+			'tags'  : ['Website', 'All']
+			},
+
+			{
+			'title' : 'My new website',
+			'description'   : 'Next version of my website.',
+			'thumbnail' : ['img/portfolio/small/s4s.jpg', 'img/portfolio/small/s3s.jpg', 'img/portfolio/small/s3s.jpg'],
+			'large' : ['img/portfolio/large/s4.jpg', 'img/portfolio/large/s3.jpg', 'img/portfolio/large/s3.jpg'],
+			'button_list'   :
+			[
+			{ 'title':'Demo', 'url' : 'http://dmytrodoianov.github.io/' }
+			],
+			'tags'  : ['Website','All']
+			},
+
+			{
+			'title' : 'Logo for ukrainian patrol police',
+			'description'   : 'By studing i design thouse logos ',
+			'thumbnail' : ['img/portfolio/small/pks.jpg', 'img/portfolio/small/pos.jpg', 'img/portfolio/small/pls.jpg', 'img/portfolio/small/pks.jpg'],
+			'large' : ['img/portfolio/large/pk.jpg', 'img/portfolio/large/po.jpg', 'img/portfolio/large/pl.jpg', 'img/portfolio/large/pk.jpg'],
+			'button_list'   :
+			[
+			{ 'title':'Demo', 'url' : 'http://dmytrodoianov.github.io/' }
+			],		
+			'tags'  : ['Photoshop','All']
+			},
+
+			{
+			'title' : 'My Logo',
+			'description'   : 'I work with soul on it and it is greath!',
+			'thumbnail' : ['img/portfolio/small/logos.jpg'],
+			'large' : ['img/portfolio/large/logo.jpg'],
+			'button_list'   :
+			[
+			{ 'title':'Demo', 'url' : 'http://dmytrodoianov.github.io/' }
+			],			
+			'tags'  : ['Photoshop','All']
+			},
+
+			{
+			'title' : 'Treasure and girl',
+			'description'   : 'Girl, chest and fog. What else need for romantic?',
+			'thumbnail' : ['img/portfolio/small/collage2s.jpg'],
+			'large' : ['img/portfolio/large/collage2.jpg'],
+			'button_list'   :
+			[
+			{ 'title':'Demo', 'url' : 'http://dmytrodoianov.github.io/' }
+			],
+			'tags'  : ['Photoshop','All']
+			},
+
+			{
+			'title' : 'Fitness website',
+			'description'   : 'For futere website of my gym.',
+			'thumbnail' : ['img/portfolio/small/s1s.jpg'],
+			'large' : ['img/portfolio/large/s1.jpg'],
+			'button_list'   :
+			[
+			{ 'title':'Demo', 'url' : 'http://dmytrodoianov.github.io/demo/fitness/index.html' }		
+			],
+			'tags'  : ['Website','All']
+			},
+
+			{
+			'title' : 'Eagle in fantasy world',
+			'description'   : 'American whitehead eagle and some wariation of fantasy world',
+			'thumbnail' : ['img/portfolio/small/collage1s.jpg'],
+			'large' : ['img/portfolio/large/collage1.jpg'],
+			'button_list'   :
+			[
+			{ 'title':'Demo', 'url' : 'http://dmytrodoianov.github.io/' }
+			],			
+			'tags'  : ['Photoshop','All']
+			},
+
+			{
+			'title' : 'Aion',
+			'description'   : 'December 11 of 2009 Was started an open beta testing of rusian localization of the game "Aion". I was one of the first players. ',
+			'thumbnail' : ['img/portfolio/small/css.jpg', 'img/portfolio/small/css.jpg', 'img/portfolio/small/css.jpg', 'img/portfolio/small/css.jpg'],
+			'large' : ['img/portfolio/large/cs.jpg', 'img/portfolio/large/cs.jpg', 'img/portfolio/large/cs.jpg', 'img/portfolio/large/cs.jpg'],
+			'button_list'   :
+			[
+			{ 'title':'Demo', 'url' : 'http://dmytrodoianov.github.io/' }
+			],			
+			'tags'  : ['Games','All' ]
+			},
+
+			{
+			'title' : 'Ship collage',
+			'description'   : 'Moment from the pirates of the Caribean.',
+			'thumbnail' : ['img/portfolio/small/collage3s.jpg'],
+			'large' : ['img/portfolio/large/collage3.jpg'],
+			'button_list'   :
+			[
+			{ 'title':'Demo', 'url' : 'http://dmytrodoianov.github.io/' }
+			],			
+			'tags'  : ['Photoshop','All']
+			},
+
+			{
+			'title' : 'Coollest collage',
+			'description'   : 'Impressions)',
+			'thumbnail' : ['img/portfolio/small/collage4s.jpg'],
+			'large' : ['img/portfolio/large/collage4.jpg'],
+			'button_list'   :
+			[
+			{ 'title':'Demo', 'url' : 'http://dmytrodoianov.github.io/' }
+			],
+			'tags'  : ['Photoshop','All']
+			},
+			
+			{
+			'title' : 'Lineage Innova obt',
+			'description'   : 'After months of anxious waiting Innova finally launches closed beta test on the official server Lineage II for CIS.  Ardent fans of the game around the clock sitting on the game forum and waiting for news from the organizers. November 1st for participants who qualified to participate in the PTA were sent to unique keys for access to the test server, and I was one of the lucky ones)',
+			'thumbnail' : ['img/portfolio/small/css.jpg', 'img/portfolio/small/ci-2s.jpg', 'img/portfolio/small/ci-3s.jpg', 'img/portfolio/small/ci-4s.jpg', 'img/portfolio/small/ci-5s.jpg'],
+			'large' : ['img/portfolio/large/cs.jpg', 'img/portfolio/large/ci-2.jpg', 'img/portfolio/large/ci-3.jpg', 'img/portfolio/large/ci-4.jpg', 'img/portfolio/large/ci-5.jpg'],
+			'button_list'   :
+			[
+			{ 'title':'Demo', 'url' : 'http://dmytrodoianov.github.io/' }
+			],			
+			'tags'  : ['Games', 'All']
+			},
+			
+			{
+			'title' : 'Posters for the shop Digital',
+			'description'   : 'Inner and outer design graphic for shops posters.',
+			'thumbnail' : ['img/portfolio/small/s5s.jpg'],
+			'large' : ['img/portfolio/large/s5.jpg'],
+			'button_list'   :
+			[
+			{ 'title':'Demo', 'url' : 'http://dmytrodoianov.github.io/' }
+			],			
+			'tags'  : ['Photoshop','All']
+			},
+			
+			{
+			'title' : 'Ease of use website design',
+			'description'   : 'Simple html based website.',
+			'thumbnail' : ['img/portfolio/small/s5s.jpg'],
+			'large' : ['img/portfolio/large/s5.jpg'],
+			'button_list'   :
+			[
+			{ 'title':'Demo', 'url' : 'http://dmytrodoianov.github.io/demo/ed/index.html' }
+			],
+			'tags'  : ['Website','All']
+			},
+
+			{
+			'title' : 'LineAge II - Oath of Blood obt' ,
+			'description'   : 'Long time ago, I was take a part of beta testing new chronicles 5 of Lineage 2.',
+			'thumbnail' : ['img/portfolio/small/c5-1s.jpg', 'img/portfolio/small/c5-2s.jpg', 'img/portfolio/small/c5-3s.jpg', 'img/portfolio/small/c5-4s.jpg','img/portfolio/small/c5-5s.jpg'],
+			'large' :     ['img/portfolio/large/c5-1.jpg', 'img/portfolio/large/c5-2.jpg', 'img/portfolio/large/c5-3.jpg', 'img/portfolio/large/c5-4.jpg', 'img/portfolio/large/c5-5s.jpg'],
+			'button_list'   :
+			[
+			{ 'title':'Demo', 'url' : 'http://dmytrodoianov.github.io/' }
+			],			
+			'tags'  : ['Games','All']
+			},
+{
+			'title' : 'Collage for spring girls',
+			'description'   : '=)',
+			'thumbnail' : ['img/portfolio/small/fg1s.jpg','img/portfolio/small/fg2s.jpg'],
+			'large' :     ['img/portfolio/large/fg1.jpg','img/portfolio/large/fg2.jpg'],
+			'button_list'   :
+			[
+			{ 'title':'Demo', 'url' : 'http://dmytrodoianov.github.io/' }
+			],			
+			'tags'  : ['Photoshop','All']
+			},
+			
+			{
+			'title' : 'LineAge II - Scions of Desteny obt',
+			'description'   : 'Long time ago, I was take a part of beta testing new chronicles 4 of Lineage 2.',
+			'thumbnail' : ['img/portfolio/small/c4-1s.jpg', 'img/portfolio/small/c4-2s.jpg', 'img/portfolio/small/c4-3s.jpg', 'img/portfolio/small/c4-4s.jpg'],
+			'large' :     ['img/portfolio/large/c4-1.jpg', 'img/portfolio/large/c4-2.jpg', 'img/portfolio/large/c4-3.jpg', 'img/portfolio/large/c4-4.jpg'],
+			'button_list'   :
+			[
+			{ 'title':'Demo', 'url' : 'http://dmytrodoianov.github.io/' }
+			],			
+			'tags'  : ['Games', 'All']
+			}
+		]
+	});
+});
